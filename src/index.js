6ff42b1041f292984e6cbe4598137ce4254ac85d
alejandro.bullrich <alejandro.bullrich@playtika.com>
2019-10-17 21:25:32 -0300

    Adds appBar and table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ZeusAppBar from "./ButtonAppBar";
+import ZeusTable from "./SimpleTable";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <ZeusAppBar/>
+        <ZeusTable/>
+    </div>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

c943c3902b785e9622689d6104d0067e7fca677a
alejandro.bullrich <alejandro.bullrich@playtika.com>
2019-10-21 20:59:23 -0300

    * Adds app logo. * Adds responsive sidebar.

--- src/SimpleTable.js
+++ src/SimpleTable.js
@@ -25,16 +25,22 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+class Entity {
+    constructor(name, owner, lastModification, fileSize) {
+        this.name = name;
+        this.owner = owner;
+        this.lastModification = lastModification;
+        this.fileSize = fileSize;
+    }
+}
+
 function createData(name, calories, fat, carbs, protein) {
     return {name, calories, fat, carbs, protein};
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    new Entity('Archivo0', 'Alejandro Bullrich', '17/07/2019', 24 + 'MB'),
+    new Entity('Archivo1', 'Macarena Lui', '12/06/2019', 24 + 'MB'),
 ];
 
 function SimpleTable() {
@@ -45,11 +51,10 @@ function SimpleTable() {
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Nombre</TableCell>
+                        <TableCell align="right">Propietario</TableCell>
+                        <TableCell align="right">&Uacute;ltima Mofificaci&oacute;n</TableCell>
+                        <TableCell align="right">Tamaño del archivo</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -58,10 +63,9 @@ function SimpleTable() {
                             <TableCell component="th" scope="row">
                                 {row.name}
                             </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="right">{row.owner}</TableCell>
+                            <TableCell align="right">{row.lastModification}</TableCell>
+                            <TableCell align="right">{row.fileSize}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
